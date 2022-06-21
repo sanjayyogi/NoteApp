19468f9361239db6adc781e62c006ce7059c3a23
sachinjangir99 <sachinladusar@gmail.com>
2022-06-21 17:05:55 +0530

    Updat

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 showNotes();
 
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function (e) {
+addBtn.addEventListener("click", function(e){
   let addTxt = document.getElementById("addText");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  let myObj = {
+      title: addTitle.value,
+      text: addText.value
+  }
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
 
   showNotes();
 });
@@ -29,8 +35,8 @@ function showNotes() {
     html += `       
           <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
               <div class="card-body">
-                <h5 class="card-title">Note ${index + 1}</h5>
-                <p class="card-text">${element}</p>
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${element.text}</p>
                 <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
               </div>
             </div>`;
@@ -73,4 +79,4 @@ search.addEventListener("input",function(){
             element.style.display = "none";
         }
     })
-})
+}) 
